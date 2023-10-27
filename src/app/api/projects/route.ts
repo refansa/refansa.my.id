656cadf9cefbbb86b8207fe5123bd30eab4e72ff
Refansa <m.refansa.am@gmail.com>
2023-10-27 19:47:27 +0700

    fix fetch

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import data from '../../data/projectList.json'
+import data from '../../../data/projectList.json'
 
 export async function GET() {
   return NextResponse.json(data)
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,13 @@
 import { Metadata } from 'next'
 import Project from './project'
+import data from '../../data/projectList.json'
 
 export const metadata: Metadata = {
   title: 'Projects',
 }
 
 async function getProjectLists() {
-  const baseURL = process.env.APP_URL ?? 'https://refansa.vercel.app'
-  const res = await fetch(`${baseURL}/api/projects`)
-
-  if (!res.ok) throw new Error('Failed to fetch project lists.')
-
-  return res.json()
+  return data
 }
 
 export default async function Projects() {

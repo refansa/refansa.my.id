7bd2e4702412f8b98e12dc5dcd448014b4e7c505
refansa <m.refansa.am@gmail.com>
2024-08-03 10:42:25 +0700

    chore: initial mdx integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextMDX from '@next/mdx'
 
-export default nextConfig;
+const withMDX = nextMDX()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    // Use the mdx rust compiler
+    mdxRs: true,
+  },
+}
+
+export default withMDX(nextConfig)
